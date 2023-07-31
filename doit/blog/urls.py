a4cc6e6a3b839c697122a4af5e6c3c5d27db11e1
Seojune Lee <59250751+vantaa89@users.noreply.github.com>
2023-07-31 11:37:56 +0000

    FBV로 블로그 포스트 목록 페이지 만들기

--- doit/blog/urls.py
+++ doit/blog/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index), # views.py에 정의된 index() 함수를 호출
 ]
--- doit/blog/views.py
+++ doit/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def index(request):
+    # render함수를 이용해 templates 폴더의 blog/index.html을 표시해줌
+    posts = Post.objects.all().order_by('-pk')
+    return render(
+        request,
+        'blog/index.html', 
+        {
+            "posts": posts,
+        }
+    )
